96cbe660da00bbddacbcbdfd8ef461db7038d81d
jbienzms <jbienzms@hotmail.com>
2015-05-14 14:39:23 -0500

    FeatureEngine started. Addin currently shows menu item in Tools menu and shows an empty WPF dialog.

---
+++ VisualStudio/VSFeatureEngine/Commands/ManageFeaturesCommand.cs
@@ -0,0 +1,120 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="ManageFeaturesCommand.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace VSFeatureEngine
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class ManageFeaturesCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 4129;
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly Package package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ManageFeaturesCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        private ManageFeaturesCommand(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
+
+            this.package = package;
+
+            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                CommandID menuCommandID = new CommandID(MenuGuids.MainMenuGroup, CommandId);
+                EventHandler eventHandler = this.ShowDialog;
+                MenuCommand menuItem = new MenuCommand(eventHandler, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static ManageFeaturesCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static void Initialize(Package package)
+        {
+            Instance = new ManageFeaturesCommand(package);
+        }
+
+        /// <summary>
+        /// Shows a message box when the menu item is clicked.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void ShowMessageBox(object sender, EventArgs e)
+        {
+            // Show a Message Box to prove we were here
+            IVsUIShell uiShell = (IVsUIShell)Package.GetGlobalService(typeof(SVsUIShell));
+            Guid clsid = Guid.Empty;
+            int result;
+            Microsoft.VisualStudio.ErrorHandler.ThrowOnFailure(uiShell.ShowMessageBox(
+                0,
+                ref clsid,
+                "FeatureEnginePackage",
+                string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName),
+                string.Empty,
+                0,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST,
+                OLEMSGICON.OLEMSGICON_INFO,
+                0,        // false
+                out result));
+        }
+
+        /// <summary>
+        /// Shows the tool window when the menu item is clicked.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event args.</param>
+        private void ShowDialog(object sender, EventArgs e)
+        {
+            // Create and show the dialog
+            var dlg = new ManageFeaturesDialog();
+            var m = dlg.ShowModal();
+        }
+    }
+}
---
+++ VisualStudio/VSFeatureEngine/Commands/MenuGuids.cs
@@ -0,0 +1,21 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="FeatureEnginePackageGuids.cs" company="Company">
+//         Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace VSFeatureEngine
+{
+    using System;
+
+    /// <summary>
+    /// FeatureEnginePackage GUID constants.
+    /// </summary>
+    internal static class MenuGuids
+    {
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid MainMenuGroup = new Guid("28b97737-a1b4-4bd6-b55c-ad4ef63a1c51");
+    }
+}
