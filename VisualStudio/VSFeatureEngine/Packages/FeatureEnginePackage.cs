ceb53b339287ce88251402901300aeed85c94f9d
jbienzms <jbienzms@hotmail.com>
2015-07-09 13:22:03 -0500

    Fixed missing files due to folder name exclusion in GitIgnore.

---
+++ VisualStudio/VSFeatureEngine/Packages/FeatureEnginePackage.cs
@@ -0,0 +1,131 @@
+﻿#region License
+/******************************************************************************
+ * COPYRIGHT © MICROSOFT CORP. 
+ * MICROSOFT LIMITED PERMISSIVE LICENSE (MS-LPL)
+ * This license governs use of the accompanying software. If you use the software, you accept this license. If you do not accept the license, do not use the software.
+ * 1. Definitions
+ * The terms "reproduce," "reproduction," "derivative works," and "distribution" have the same meaning here as under U.S. copyright law.
+ * A "contribution" is the original software, or any additions or changes to the software.
+ * A "contributor" is any person that distributes its contribution under this license.
+ * "Licensed patents" are a contributor's patent claims that read directly on its contribution.
+ * 2. Grant of Rights
+ * (A) Copyright Grant- Subject to the terms of this license, including the license conditions and limitations in section 3, each contributor grants you a non-exclusive, worldwide, royalty-free copyright license to reproduce its contribution, prepare derivative works of its contribution, and distribute its contribution or any derivative works that you create.
+ * (B) Patent Grant- Subject to the terms of this license, including the license conditions and limitations in section 3, each contributor grants you a non-exclusive, worldwide, royalty-free license under its licensed patents to make, have made, use, sell, offer for sale, import, and/or otherwise dispose of its contribution in the software or derivative works of the contribution in the software.
+ * 3. Conditions and Limitations
+ * (A) No Trademark License- This license does not grant you rights to use any contributors' name, logo, or trademarks.
+ * (B) If you bring a patent claim against any contributor over patents that you claim are infringed by the software, your patent license from such contributor to the software ends automatically.
+ * (C) If you distribute any portion of the software, you must retain all copyright, patent, trademark, and attribution notices that are present in the software.
+ * (D) If you distribute any portion of the software in source code form, you may do so only under this license by including a complete copy of this license with your distribution. If you distribute any portion of the software in compiled or object code form, you may only do so under a license that complies with this license.
+ * (E) The software is licensed "as-is." You bear the risk of using it. The contributors give no express warranties, guarantees or conditions. You may have additional consumer rights under your local laws which this license cannot change. To the extent permitted under your local laws, the contributors exclude the implied warranties of merchantability, fitness for a particular purpose and non-infringement.
+ * (F) Platform Limitation- The licenses granted in sections 2(A) & 2(B) extend only to the software or derivative works that you create that run on a Microsoft Windows operating system product.
+ ******************************************************************************/
+#endregion // License
+
+using System;
+using System.Activities;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows;
+using Microsoft.FeatureEngine;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.Win32;
+using NuGet.VisualStudio;
+using C = Microsoft.VisualStudio.VSConstants;
+
+namespace VSFeatureEngine
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the 
+    /// IVsPackage interface and uses the registration attributes defined in the framework to 
+    /// register itself and its components with the shell. These attributes tell the pkgdef creation
+    /// utility what data to put into .pkgdef file.
+    /// </para>
+    /// <para>
+    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+    /// </para>
+    /// </remarks>
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [Guid(FeatureEnginePackageGuids.PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    [ProvideMenuResource("Menus.ctmenu", 2)]
+    [ProvideAutoLoad(C.UICONTEXT.SolutionExists_string)]
+    // [ProvideAutoLoad(C.UICONTEXT.NoSolution_string)]
+    public sealed class FeatureEnginePackage : Package
+    {
+        #region Member Variables
+        private IFeatureManager featureManager;
+        private IVsPackageInstallerEvents installerEvents;
+        private ServiceStore store;
+        #endregion // Member Variables
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FeatureEnginePackage"/> class.
+        /// </summary>
+        public FeatureEnginePackage()
+        {
+            // Inside this method you can place any initialization code that does not require 
+            // any Visual Studio service because at this point the package object is created but 
+            // not sited yet inside Visual Studio environment. The place to do all the other 
+            // initialization is the Initialize method.
+        }
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            // Make sure the plugin doesn't fail to load
+            TaskHelper.RunWithErrorHandling(() =>
+            {
+                // Resolve our service store
+                store = ((IComponentModel)GetService(typeof(SComponentModel))).GetService<ServiceStore>();
+
+                // Initialize the service store
+                store.Initialize(this);
+
+                // Resolve Feature Manager itself
+                var ifm = store.GetService<IFeatureManager>();
+                var fm = store.GetService<FeatureManager>();
+
+                // Initialize Feature Manager
+                fm.Initialize();
+
+                // Initialize menu placeholders
+                ActionsMenu.Initialize(this);
+
+                // Resolve the solution watcher
+                var watcher = store.GetService<SolutionWatcher>();
+
+                // Initialize the watcher
+                watcher.Initialize();
+
+                // Initalize standard commands
+                // ManageFeaturesCommand.Initialize(this);
+
+            }, TaskRunOptions.WithFailure("Could not initialize the Feature Engine plugin"));
+        }
+
+        #endregion
+    }
+}
